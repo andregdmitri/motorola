136d151b0f13292090eef837310af61ac9e749a8
André Guarnier De Mitri <andregdmitri@gmail.com>
2025-11-05 00:26:22 -0300

    feat: new constants for thrshold unusual propns

--- utils/constants.py
+++ utils/constants.py
@@ -9,3 +9,5 @@ DEFAULT_SPACY_MODEL = os.getenv("DEFAULT_SPACY_MODEL", "en_core_web_sm") # CPU
 DEFAULT_OUTPUT_DIR = os.getenv("DEFAULT_OUTPUT_DIR", "data/output/")
 DEFAULT_SEED = int(os.getenv("DEFAULT_SEED", "42"))
 SAMPLE_SIZE = int(os.getenv("SAMPLE_SIZE", "1000"))
+FREQ_THRESHOLD = int(os.getenv("FREQ_THRESHOLD", "5"))  # Frequency threshold for unusual PROPNs
+SCORE_THRESHOLD = float(os.getenv("SCORE_THRESHOLD", "0.5")) # Threshold for unusual PROPNs
