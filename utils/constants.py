c14c466e11defde23596e5288de9aad032d748eb
André Guarnier De Mitri <andregdmitri@gmail.com>
2025-11-05 10:06:48 -0300

    feat: multiprocessing constants

--- utils/constants.py
+++ utils/constants.py
@@ -11,3 +11,6 @@ DEFAULT_SEED = int(os.getenv("DEFAULT_SEED", "42"))
 SAMPLE_SIZE = int(os.getenv("SAMPLE_SIZE", "1000"))
 FREQ_THRESHOLD = int(os.getenv("FREQ_THRESHOLD", "5"))  # Frequency threshold for unusual PROPNs
 SCORE_THRESHOLD = float(os.getenv("SCORE_THRESHOLD", "0.5")) # Threshold for unusual PROPNs
+NUM_CORES = int(os.getenv("NUM_CORES", "-1"))
+BATCH_SIZE = int(os.getenv("BATCH_SIZE", "32"))
+NON_ENGLISH_THRESHOLD = float(os.getenv("NON_ENGLISH_THRESHOLD", "0.75"))
